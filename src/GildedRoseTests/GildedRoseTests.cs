6cc01c385657e5d69aba41da33a735accdda328f
Joseph Egglestone <da2jeg@googlemail.com>
2025-12-05 07:56:34 +0000

    Add tests to show that items decrease in quality and sellInDays

--- src/GildedRoseTests/GildedRoseTests.cs
+++ src/GildedRoseTests/GildedRoseTests.cs
@@ -1,4 +1,5 @@
 ﻿using GildedRoseKata;
+using System.Diagnostics.Contracts;
 
 namespace GildedRoseTests;
 
@@ -11,5 +12,23 @@ public class GildedRoseTests
         GildedRose app = new(items);
         app.UpdateQuality();
         Assert.Equal("foo", items[0].Name);
+    }
+
+    [Fact]
+    public void EachDayNormalItemDecreasesInQuality()
+    {
+        List<Item> items = [new Item { Name = "Normal Item", SellInDays = 5, Quality = 10 }];
+        GildedRose app = new(items);
+        app.UpdateQuality();
+        Assert.Equal(9, items[0].Quality);
+    }
+
+    [Fact]
+    public void EachDayNormalItemDecreasesSellInDays()
+    {
+        List<Item> items = [new Item { Name = "Normal Item", SellInDays = 5, Quality = 10 }];
+        GildedRose app = new(items);
+        app.UpdateQuality();
+        Assert.Equal(4, items[0].SellInDays);
     }
 }
