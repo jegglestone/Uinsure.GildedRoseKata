47ed8ceba92067d339e1ecb53128c9f5bbe12ccb
Joseph Egglestone <da2jeg@googlemail.com>
2025-12-05 09:58:43 +0000

    Backstage pass quality increases by 3 when 5 days or less

--- src/GildedRoseTests/GildedRoseTests.cs
+++ src/GildedRoseTests/GildedRoseTests.cs
@@ -91,6 +91,24 @@ public class GildedRoseTests
     }
 
     [Fact]
+    public void BackstagePassesIncreaseInQualityBy3WhenThereAre5SellInDaysLeft()
+    {
+        List<Item> items = [new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellInDays = 5, Quality = 30 }];
+        GildedRose app = new(items);
+        app.UpdateQuality();
+        Assert.Equal(33, items[0].Quality);
+    }
+
+    [Fact]
+    public void BackstagePassesIncreaseInQualityBy3WhenThereAreLessThan5SellInDaysLeft()
+    {
+        List<Item> items = [new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellInDays = 4, Quality = 30 }];
+        GildedRose app = new(items);
+        app.UpdateQuality();
+        Assert.Equal(33, items[0].Quality);
+    }
+
+    [Fact]
     public void BackstagePassesQualityDropsToZeroAfterConcert()
     {
         List<Item> items = [new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellInDays = 0, Quality = 20 }];
