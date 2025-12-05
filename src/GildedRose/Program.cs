33a3c779244acd2b7fe52f2d05a443bb474773ac
Joseph Egglestone <da2jeg@googlemail.com>
2025-12-05 07:28:39 +0000

    Update namespaces

--- src/GildedRose/Program.cs
+++ src/GildedRose/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace GildedRoseKata;
+﻿namespace GildedRose;
 
 public class Program
 {
@@ -46,7 +46,7 @@ public class Program
             Console.WriteLine("name, sellIn, quality");
             for (var j = 0; j < Items.Count; j++)
             {
-                System.Console.WriteLine(Items[j].Name + ", " + Items[j].SellInDays + ", " + Items[j].Quality);
+                Console.WriteLine(Items[j].Name + ", " + Items[j].SellInDays + ", " + Items[j].Quality);
             }
             Console.WriteLine("");
             app.UpdateQuality();
--- src/GildedRoseTests/GildedRoseTest.cs
+++ src/GildedRoseTests/GildedRoseTest.cs
@@ -1,4 +1,4 @@
-﻿using GildedRoseKata;
+﻿using GildedRose;
 
 namespace GildedRoseTests;
 
